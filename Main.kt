0e46bff84caf480ce1260aef20fd021abb2d3f7f
Akobirxuja <108734078+alikhanov17@users.noreply.github.com>
2022-07-30 08:21:03 +0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+    val arr = arrayOf(10,1)
+    val arr2 = arrayOf(10,2)
+    var res = 0
+    var res2 = 0
+
+    for (e in arr.indices){
+        res+=arr[e]
+
+
+
+    }
+    for (e in arr2.indices){
+        res2+=arr2[e]
+
+
+
+    }
+
+    println(res+res2)
+
+
+
+
+}
